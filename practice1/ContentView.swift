05c7e52d34ca3c7a4d764ccd3f65fe0a4f5cfcf8
stationAstral <cougarjonez@gmail.com>
2023-10-24 19:35:46 -0700

    Initial Commit

---
+++ practice1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practice1
+//
+//  Created by stationAstral on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practice1/practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  practice1App.swift
+//  practice1
+//
+//  Created by stationAstral on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
